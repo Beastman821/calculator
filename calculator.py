8f956c67bde42312d4697573d7c9cbc92ce44874
Beastman821 <86630145+Beastman821@users.noreply.github.com>
2021-11-10 16:47:37 -0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,10 @@
+import math
+
+firstNumber = int(input('What is your first number? '))
+
+mathOp = ['*', '/', '+', '-']
+
+mathOporations = input(f"Which math operation do you want? {mathOp}")
+
+secondNumber = int(input('What is your second number? '))
+
